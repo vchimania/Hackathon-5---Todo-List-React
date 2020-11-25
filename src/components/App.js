e503d03b5307f5458497366d19b52dca6475b62b
John DoeVini <vinichimania98@gmail.com>
2020-11-25 23:02:51 +0530

    Final todo list

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,42 @@
 import React, { useState } from "react";
+import ListItem from './ListItem';
 import "./../styles/App.css";
-import ListItem from "./ListItem";
-
-
 
 function App() {
-	const[items,setItems]=useState([]);
-	const[newItem,setNewItem]=useState("");
-	const addItem=() =>{
-		items.push(newItem);
-		setItems([...items]);
-		setNewItem("");
-	}
-	const newItemChanged=(evt) =>{
-		setNewItem(evt.target.value);
-	}
-
-	const deleteHandler= (itemIdx) =>{
-		items.splice(itemIdx,1);
-		setItems([...items]);
-	}
-	const editHandler=(editedValue,itemIdx)=>{
-		item[itemIdx]=editedValue;
-		setItems([...items]);
-	}
-	return (
-	<div id="main">
-	<textarea id="task" onChange={newItemChanged} placeholder="New Item" value={newItem}>
-	</textarea>
-	<button id="btn" onClick={addItem} disabled={newItem.trim().length=== 0}>Add Item</button>
-	{
-		items.map((item,idx)=>(
-			<ListItem item={item} key={`$(item)_$(idx)`} id={idx} editHandler={editHandler} deleteHandler={deleteHandler}/>
-		))
+const [items, setItems] = useState([]);
+const [newItem, setNewItem] = useState("");
+const addItem = () => {
+	items.push(newItem);
+	setItems([...items]);
+    setNewItem("");
 }
-	</div>
+const newItemChanged = (evt) => {
+	setNewItem(evt.target.value);
+}
+const editHandler = (editedValue,itemIdx) => {
+	items[itemIdx] = editedValue;
+	setItems([...items])
+  }
+const deleteHandler = (itemIdx) => {
+  items.splice(itemIdx, 1);
+  setItems([...items])
+}
+	return (
+	 <div id="main">
+	<textarea id="task" onChange={newItemChanged} placeholder="New Item" value={newItem}></textarea>
+	<button id="btn" onClick={addItem} 
+	disabled={newItem.trim().length === 0}>Add Item</button>
+	{items.map((item ,idx) => (
+		<ListItem item={item} 
+		key={`${item}_${idx}`} 
+		idx={idx} 
+		editHandler={editHandler} 
+		deleteHandler={deleteHandler}/>
+		))}
+	 </div>
 	);
+
 }
 
 
 export default App;
-
